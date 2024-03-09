9f9768d36e356ac3408ee16b22865386bd160f0f
juann227 <juanjoruizt@gmail.com>
2024-03-09 17:28:23 -0500

    intento de roundrobin

--- UR_OS/src/ur_os/RoundRobin.java
+++ UR_OS/src/ur_os/RoundRobin.java
@@ -27,15 +27,43 @@ public class RoundRobin extends Scheduler{
    
     @Override
     public void getNext(boolean cpuEmpty) {
-        
+        if(!processes.isEmpty()){
+            Process apuntador = null;
+            if (cpuEmpty) {
+                Process p = processes.get(0);
+                processes.remove();
+                os.interrupt(InterruptType.SCHEDULER_RQ_TO_CPU, p);
+            }else{
+                Process proc = os.cpu.getProcess();
+                for(Process pr : processes){
+                    if (cont < q) {
+                        apuntador = pr;
+                        os.interrupt(InterruptType.SCHEDULER_RQ_TO_CPU, apuntador);
+                        cont++;
+                        processes.remove(apuntador);
+                    }
+                    if (proc.getRemainingTimeInCurrentBurst() != 0){
+                        os.interrupt(InterruptType.SCHEDULER_CPU_TO_RQ, apuntador);
+                        processes.add(apuntador);
+                    }
+                }
+                
+            }
+        }
         
         
     }
     
     @Override
-    public void newProcess(boolean cpuEmpty) {} //It's empty because it is Non-preemptive
+    public void newProcess(boolean cpuEmpty) {
+        //the process goes to the ready queue
+        Process p1 =  os.cpu.getProcess();
+        os.cpu.removeProcess();
+        os.rq.addProcess(p1);
+    } 
 
     @Override
-    public void IOReturningProcess(boolean cpuEmpty) {} //It's empty because it is Non-preemptive
+    public void IOReturningProcess(boolean cpuEmpty) {
+    } 
     
 }
